0b8323c3fcb89c4713ed8d57fc07eda8829e466e
Paweł Pustelnik <pustypawel@gmail.com>
2020-12-05 16:14:17 +0100

    domyślny displayname generator

--- src/test/java/pl/edu/wszib/order/OrderFacadeTest.java
+++ src/test/java/pl/edu/wszib/order/OrderFacadeTest.java
@@ -1,7 +1,6 @@
 package pl.edu.wszib.order;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -20,7 +19,6 @@ class OrderFacadeTest {
     }
 
     @Test
-    @DisplayName("should be able to create order")
     public void should_be_able_to_create_order() {
         // given: We have simple, correct order
         final Order correctOrder = correctOrder();
@@ -33,51 +31,43 @@ class OrderFacadeTest {
     }
 
     @Test
-    @DisplayName("should not be able to create order with the same id")
     public void should_not_be_able_to_create_order_with_the_same_id() {
         fail();
     }
 
     @Test
-    @DisplayName("should be able to get existing order by id")
     public void should_be_able_to_get_existing_order_by_id() {
         fail();
     }
 
     @Test
-    @DisplayName("should not be able to get not existing order by id")
     public void should_not_be_able_to_get_not_existing_order_by_id() {
         fail();
     }
 
     @Test
-    @DisplayName("should be able to update order")
     public void should_be_able_to_update_order() {
         fail();
     }
 
     @Test
-    @DisplayName("should not be able to update not existing order")
     public void should_not_be_able_to_update_not_existing_order() {
         fail();
     }
 
     @Test
-    @DisplayName("should be able to add position to order")
     public void should_be_able_to_add_position_to_order() {
         // TODO: test, implementacja, wymyślić inne testy
         fail();
     }
 
     @Test
-    @DisplayName("should be able to remove position from order")
     public void should_be_able_to_remove_position_from_order() {
         // TODO: test, implementacja, wymyślić inne testy
         fail();
     }
 
     @Test
-    @DisplayName("should be able to submit order")
     public void should_be_able_to_submit_order() {
         // TODO: test, implementacja, wymyślić inne testy
         fail();
