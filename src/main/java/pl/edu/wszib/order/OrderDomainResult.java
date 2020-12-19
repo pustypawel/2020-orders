fe678189120f3033aa6615c733f1e2bb08c7b389
Paweł Pustelnik <pustypawel@gmail.com>
2020-12-19 17:13:32 +0100

    fix: wprowadzenie OrderDomainResult

--- src/main/java/pl/edu/wszib/order/OrderDomainResult.java
+++ src/main/java/pl/edu/wszib/order/OrderDomainResult.java
@@ -1,24 +1,23 @@
 package pl.edu.wszib.order;
 
 import lombok.Value;
-import pl.edu.wszib.order.dto.OrderDto;
 
 @Value
 class OrderDomainResult {
     OrderResultType type;
     Order order;
 
-    public static OrderResult failure(OrderResultType type) {
+    public static OrderDomainResult failure(OrderResultType type) {
         if (type == OrderResultType.OK) {
             throw new IllegalArgumentException("type can't be " + OrderResultType.OK);
         }
-        return new OrderResult(type, null);
+        return new OrderDomainResult(type, null);
     }
 
-    public static OrderResult success(OrderDto order) {
-        return new OrderResult(OrderResultType.OK, order);
+    public static OrderDomainResult success(Order order) {
+        return new OrderDomainResult(OrderResultType.OK, order);
     }
-    
+
     public boolean isSuccess() {
         return type == OrderResultType.OK;
     }
--- src/main/java/pl/edu/wszib/order/OrderResultType.java
+++ src/main/java/pl/edu/wszib/order/OrderResultType.java
@@ -3,5 +3,5 @@ package pl.edu.wszib.order;
 public enum OrderResultType {
     OK,
     ALREADY_EXIST,
-    NOT_FOUND;
+    NOT_FOUND, ALREADY_SUBMITTED;
 }
