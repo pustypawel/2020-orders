3e5187add0dc5ee4d2e79fc95e5f9e529669b78c
Paweł Pustelnik <pustypawel@gmail.com>
2021-01-16 19:04:39 +0100

    spring

---
+++ order-infrastructure/src/main/java/pl/edu/wszib/orderinfrastructure/OrderInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.orderinfrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderInfrastructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderInfrastructureApplication.class, args);
+	}
+
+}
---
+++ order-infrastructure/src/test/java/pl/edu/wszib/orderinfrastructure/OrderInfrastructureApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.orderinfrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderInfrastructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
