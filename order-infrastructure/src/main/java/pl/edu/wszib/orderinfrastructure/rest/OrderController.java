4d08a1ef0205bde11da66deccc9cde3e1c2e93c0
Paweł Pustelnik <pustypawel@gmail.com>
2021-03-14 13:12:09 +0100

    task: impl rest

--- order-infrastructure/src/main/java/pl/edu/wszib/orderinfrastructure/rest/OrderController.java
+++ order-infrastructure/src/main/java/pl/edu/wszib/orderinfrastructure/rest/OrderController.java
@@ -2,8 +2,6 @@ package pl.edu.wszib.orderinfrastructure.rest;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import pl.edu.wszib.order.OrderFacade;
 import pl.edu.wszib.order.OrderResult;
@@ -15,9 +13,28 @@ import java.util.Collection;
 @RestController
 @RequestMapping("/api/orders")
 @AllArgsConstructor
-@Slf4j
 public class OrderController {
     private final OrderFacade orderFacade;
+
+    // 1: get one
+    // 2: submit
+    // 3: update
+    // 4: add position
+    // 5: remove position
+
+    // GET /api/orders/{orderId}
+    // POST /api/orders/{orderId}/submit
+    // PUT /api/orders/{orderId}
+    // POST /api/orders/{orderId}/positions
+    // DELETE /api/orders/{orderId}/positions/{positionNumber}
+
+//    @GetMapping
+//    @PostMapping
+//    @PutMapping
+//    @DeleteMapping
+
+//    @PathVariable
+//    @RequestParam
 
     @GetMapping
     public Collection<OrderDto> getAll() {
